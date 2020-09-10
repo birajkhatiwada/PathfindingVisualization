e156cecc26761ce2b356127a799385c26744cf4d
Biraj <khatiwadabizzz@gmail.com>
2020-09-10 01:01:28 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import pygame
+import math
+import Queue from PriorityQueue
+
+
+WIDTH = 600
+WIN = pygame.display.set_model((WIDTH, WIDTH))
+pygame.display.set_caption("Path Finding Visualization")
+
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0,255,0)
+YELLOW = (255, 255, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+PURPLE = (128, 0, 128)
+ORANGE = (255, 165, 0)
+GREY = (128, 128, 128)
+TURQUOISE = (64, 224, 208)
